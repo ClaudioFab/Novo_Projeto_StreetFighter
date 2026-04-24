e6efa86d874c12bc09338c239d5e0ef1252e5e0b
ClaudioFab <cla.rob.fabiano@gmail.com>
2026-04-24 20:11:21 -0300

    Preparação

--- src/new_projeto_streetfighter/Luta.java
+++ src/new_projeto_streetfighter/Luta.java
@@ -2,5 +2,34 @@
 package new_projeto_streetfighter;
 
 public class Luta {
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovado;
+    
+    public void marcarLuta(){
+        /*
+        Vai receber dois “lutadores” como parâmetro;
+        Somente poderá ser marcada a luta entre lutadores da mesma categoria;
+        Desafiador e desafiante devem ser lutadores diferentes;
+        Se os termos forem aceitos a luta pode ser aprovada (aprovado == true);
+        Se os termos não forem compridos, a luta não pode ser aprovada.
+        (aprovado == false).
+        */
+        
+    }
+    
+    public void lutar(){
+        /*
+        Somente poderão batalhar se a luta for aprovada (aprovado == true);
+        Somente poderá ter como resultado a vitória de um dos lutadores ou empate.
+        O resultado deve ser lançado aleatoriamente usando o seguinte exemplo:
+        Random aleatorio = new Random();
+        int vencedor = aleatorio.nextInt(3);
+        */
+        
+    }
+    
+    
     
 }
--- src/new_projeto_streetfighter/Lutador.java
+++ src/new_projeto_streetfighter/Lutador.java
@@ -2,5 +2,137 @@
 package new_projeto_streetfighter;
 
 public class Lutador {
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private double altura;
+    private double peso;
+    private String categoria;
+    private int vitoria;
+    private int derrota;
+    private int empate;
+
+    public Lutador(String nome, String nacionalidade, int idade, double altura, double peso, String categoria, int vitoria, int derrota, int empate) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.vitoria = vitoria;
+        this.derrota = derrota;
+        this.empate = empate;
+    }
+
+    //GETTER------------------------------------------------------------------
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getVitoria() {
+        return vitoria;
+    }
+
+    public int getDerrota() {
+        return derrota;
+    }
+
+    public int getEmpate() {
+        return empate;
+    }
+    
+    //SETTER------------------------------------------------------------------
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setCategoria(String categoria) {
+        /*
+        Regras da Categoria
+        Peso menor que 52kg = “Invalido”
+        Peso menor ou igual a 70 kg = “Leve”
+        Peso menor ou igual a 83 kg = “Médio”
+        Peso menor ou igual a 110 kg = “Pesado”
+        Se nenhuma das opções = “Invalido”
+        Para facilitar, pode alterar esses parâmetros (regras de exceções) diretamente no setCategoria()
+        */
+        this.categoria = categoria;
+    }
+
+    public void setVitoria(int vitoria) {
+        this.vitoria = vitoria;
+    }
+
+    public void setDerrota(int derrota) {
+        this.derrota = derrota;
+    }
+
+    public void setEmpate(int empate) {
+        this.empate = empate;
+    }
+    
+    //METODOS-----------------------------------------------------------------
+
+    public void apresentar(){
+        //Deve demostrar as informações(atributos) do lutador.
+        
+    }
+    
+    public void status(){
+        // Deve demostrar as informações de vitorias,derrotas e empates do lutador.
+        
+    }
+
+    public void venceu(){
+        // Deve computar/calcular sempre a pontuação de cada lutador.
+        //Exemplo: setVitorias(getVitorias() + 1);
+    }
+    
+    public void perdeu(){
+        // Deve computar/calcular sempre a pontuação de cada lutador.
+        //Exemplo: setDerrota(getDerrota() + 1);
+    }
+    
+    public void empatou(){
+        // Deve computar/calcular sempre a pontuação de cada lutador.
+        //Exemplo: setEmpate(getEmpate() + 1);
+    }
     
 }

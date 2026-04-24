5699102b615cd81fb57439af85da5831e70e7a60
ClaudioFab <cla.rob.fabiano@gmail.com>
2026-04-24 20:27:22 -0300

    Atualizando

--- src/new_projeto_streetfighter/Main_StreetFighter.java
+++ src/new_projeto_streetfighter/Main_StreetFighter.java
@@ -4,6 +4,11 @@ package new_projeto_streetfighter;
 public class Main_StreetFighter {
 
     public static void main(String[] args) {
+        
+        Lutador player1 = new Lutador("Ryu","Japão",30,1.69,82,null,0,0,0);
+        Lutador player2 = new Lutador("Ken","USA",32,1.79,85,null,0,0,0);
+        
+        
 
     }
     
